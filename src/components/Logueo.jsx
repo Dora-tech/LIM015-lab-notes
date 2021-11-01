97657428f5f27e1dd888037443257c6c5b5483bd
Dora-tech <dorisfrontend@gmail.com>
2021-11-01 08:11:43 -0500

    solo boton google

--- src/components/Logueo.jsx
+++ src/components/Logueo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Stack, Container, Form, Button } from "react-bootstrap";
+ import { Button } from "react-bootstrap";
 
 import firebaseApp from "../credenciales";
 import {
@@ -34,24 +34,7 @@ const Logueo = () => {
   }
 
   return (
-    <Container>
-      <Stack gap={3}>
-        <h1>{estaRegistrandose ? "Regístrate" : "inicia sesión"}</h1>
-        <Form onSubmit={submitHandler}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-
-          <Button variant="dark" type="submit">
-            {estaRegistrandose ? "Regístrate" : "inicia sesión"}
-          </Button>
-        </Form>
+  
 
         <Button
           variant="primary"
@@ -62,17 +45,7 @@ const Logueo = () => {
           Acceder con Google
         </Button>
 
-        <Button
-          style={{ width: "300px" }}
-          variant="secondary"
-          onClick={() => setEstaRegistrandose(!estaRegistrandose)}
-        >
-          {estaRegistrandose
-            ? "¿Ya tienes cuenta? Inicia sesión"
-            : "¿No tienes cuenta? Regístrate"}
-        </Button>
-      </Stack>
-    </Container>
+  
   );
 };
 
---
+++ src/components/prueba.js
@@ -0,0 +1,67 @@
+/*         <figure className='figure-login'>
+          <img className='fondo-Login1' src='./img/fondoLogin.png' id='fondoLogin1'  >
+        </figure>
+        <article className='contenedorLogin'>
+            <figure className='figure-title-login'>
+              <img src='img/pet.jpg' id='fondoPet' className='fondoAnimalPet'>
+            </figure> 
+        <form action="#" class='frmLogin'>
+        <div className="form-login-inputs">
+            <input id='email' placeholder='Email' className='btn-texto'>
+            <input className='btn-texto' type='password' name='password' id='password' placeholder='Password'>
+          </div>  
+            <button id='btnsignin' class='btn-signin'>signin</button> 
+            <span className='message'></span> 
+            <span className='txt'>You can also enter with</span>
+          <figure className='figure-google'>
+            <img src='img/gmail.png' alt='iGoogle' className='logoGmail' id='btnGoogle'>
+          </figure>    
+          
+          <span className='txt'>¿You do not have an account?</span>
+            <button id='registro' className='btn-register' ><a href='#/register'className='a-login-signup'>register</a></button>
+          </form>
+      </article>
+ */
+
+
+
+
+{/*      <Container>
+      <Stack gap={3}>
+        <h1>{estaRegistrandose ? "Regístrate" : "inicia sesión"}</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="dark" type="submit">
+            {estaRegistrandose ? "Regístrate" : "inicia sesión"}
+          </Button>
+        </Form>
+
+        <Button
+          variant="primary"
+          type="submit"
+          style={{ width: "300px" }}
+          onClick={() => signInWithRedirect(auth, googleProvider)}
+        >
+          Acceder con Google
+        </Button>
+
+        <Button
+          style={{ width: "300px" }}
+          variant="secondary"
+          onClick={() => setEstaRegistrandose(!estaRegistrandose)}
+        >
+          {estaRegistrandose
+            ? "¿Ya tienes cuenta? Inicia sesión"
+            : "¿No tienes cuenta? Regístrate"}
+        </Button>
+      </Stack>
+    </Container>  */}
